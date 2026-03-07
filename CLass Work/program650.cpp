f868c1d435d3245c4f127c0c5e330f6faa26f973
Anuj <anujpipare8@gmail.com>
2026-03-07 17:28:06 +0530

    .

---
+++ CLass Work/program650.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void Display()
+{
+    static int i = 1; // RESOLVED
+    if(i <= 4)
+    {
+        cout<<i<<"\n";
+        i++;
+        Display();
+    } 
+}
+int main()
+{
+    Display();
+    return 0;
+}
