4334f3c5e14d5cc79f16b3f81e1207ef3c82d625
Anuj <anujpipare8@gmail.com>
2026-04-02 23:29:43 +0530

    fixed

---
+++ CLass Work/program700.cpp
@@ -0,0 +1,28 @@
+// Input : 7891
+// Output : 4
+
+#include<iostream>
+using namespace std;
+
+int CountDigits(int iNo)
+{
+    int iCount = 0;
+    while(iNo != 0)
+    {
+        iCount++;
+        iNo = iNo / 10;
+    }
+    return iCount;
+}
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    cout<<"Enter number : \n";
+    cin>>iValue;
+
+    iRet = CountDigits(iValue);
+    cout<<"Number of Digits are :"<<iRet<<"\n";
+
+    return 0;
+}
